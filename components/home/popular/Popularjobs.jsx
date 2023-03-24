fcc89c2ab16f26d5f3a7e3dafa0fe13135fe51eb
Ray Anthoney <rmaxey911@gmail.com>
2023-03-23 19:31:40 -0600

    added the Popular Jobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -38,7 +38,7 @@ const Popularjobs = () => {
 
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
+          <ActivityIndicator size="large" color={COLORS.primary}/>
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
